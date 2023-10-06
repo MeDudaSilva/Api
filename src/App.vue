9a44e206602310bdb7a5928cc3a4e2ba287ecce4
MeDudaSilva <mariaeduarda.11.me4@gmail.com>
2023-10-06 15:19:27 -0300

    axios, router, css

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <script setup>
+// import { ref, onMounted } from 'vue'
+// import axios from 'axios'
+
+// const moviesGenres = ref([])
+// const TVGenres = ref([])
+
+// onMounted(async () => {
+//   let response = await axios.get('https://api.themoviedb.org/3/genre/movie/list?language=pt-BR', {
+//     headers: {
+//       Authorization: `Bearer AQUI_COLOCAR O TOKEN DE LEITURA DA API`
+//     }
+//   })
+//   moviesGenres.value = response.data.genres
+//   response =  await axios.get('https://api.themoviedb.org/3/genre/tv/list?language=pt-BR', {
+//     headers: {
+//       Authorization: `Bearer AQUI_COLOCAR O TOKEN DE LEITURA DA API`
+//     }
+//   })
+//   TVGenres.value = response.data.genres
+// })
+
 import { ref, onMounted } from 'vue'
-import axios from 'axios'
+import api from './plugins/axios'
 
 const moviesGenres = ref([])
 const TVGenres = ref([])
 
 onMounted(async () => {
-  let response = await axios.get('https://api.themoviedb.org/3/genre/movie/list?language=pt-BR', {
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNGU4ODFkN2U0NjI3NjlhMTRjNTY1MzY0NDVhOWQyYyIsInN1YiI6IjY0ZmYxNTg1MmRmZmQ4MDBjNjJjNDA3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o2TjAEj8D_SBdyWTFUpMjx0_05-mpznEny7F1pYFt74`
-    }
-  })
+  let response = await api.get('genre/movie/list?language=pt-BR')
   moviesGenres.value = response.data.genres
-  response =  await axios.get('https://api.themoviedb.org/3/genre/tv/list?language=pt-BR', {
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNGU4ODFkN2U0NjI3NjlhMTRjNTY1MzY0NDVhOWQyYyIsInN1YiI6IjY0ZmYxNTg1MmRmZmQ4MDBjNjJjNDA3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o2TjAEj8D_SBdyWTFUpMjx0_05-mpznEny7F1pYFt74`
-    }
-  })
+  response = await api.get('genre/tv/list?language=pt-BR')
   TVGenres.value = response.data.genres
 })
 </script>
 
-<template>
+<!-- <template>
   <h1>Gêneros de filmes</h1>
   <ul>
     <li v-for="genre in moviesGenres" :key="genre.id">
@@ -35,4 +48,60 @@ onMounted(async () => {
       {{ genre.name }}
     </li>
   </ul>
+</template> -->
+
+<!-- <template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filmes">Filmes</router-link>
+      <router-link to="/tv">Programas de TV</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template> -->
+
+<template>
+  <header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filmes">Filmes</router-link>
+      <router-link to="/tv">Programas de TV</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
+
+<!-- <style scoped>
+nav {
+  display: flex;
+  justify-content: flex-start;
+  margin-bottom: 1rem;
+  column-gap: 2rem;
+}
+</style> -->
+
+<style scoped>
+header {
+  height: 3rem;
+  display: flex;
+  background-color: black;
+  color: #fff;
+  font-size: 1.2rem;
+  padding-left: 2rem;
+}
+
+nav {
+  column-gap: 2rem;
+  margin-bottom: 0;
+  display: flex;
+  align-items: center;
+}
+
+nav a {
+  text-decoration: none;
+  color: #fff;
+}
+</style>
